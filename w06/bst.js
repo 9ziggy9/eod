21e0c2b6be14e0aa791fd45cc36abf82abccc810
9ziggy9 <davidarogers@protonmail.com>
2022-07-12 12:01:12 -0700

    preparing tree explanation

--- w06/bst.js
+++ w06/bst.js
@@ -42,11 +42,45 @@ function binarySearch(arr,target) {
 // When the array has EXACTLY 3 or 1 element. 1 element is WORST CASE.
 
 const arr1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-const arr2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+const arr2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
 
-console.log(binarySearch(arr1,9));
-console.log(binarySearch(arr2,9));
+// console.log(binarySearch(arr1,9));
+// console.log(binarySearch(arr2,9));
 
 // So the question becomes, how many times do I have to divide by 2 in order
 // to reduce the array down to 1 element?
 // Solve n/(2**s) = 1; <--- trivial algebra with logarithms
+
+// QUICKSORT -- divide and conquer
+function partition(arr, lo, hi) { // <-- divide
+  const pivot = arr[Math.floor((lo + hi) / 2)]; // ~ middle element
+  let l = lo; // low pointer copy
+  let h = hi; // high pointer copy
+  while (l <= h) {
+    while(arr[l] < pivot) l++; // increment up to pivot
+    while(arr[h] > pivot) h--; // decrement down to pivot
+    if (l <= h) {
+      [arr[l], arr[h]] = [arr[h], arr[l]]; // temp-less swap
+      l++;
+      h--;
+    }
+  }
+  return l;
+}
+
+// TREE recursive!
+function quickSort(arr, lo, hi) { // conquer
+  let index;
+  if (arr.length > 1) {
+    index = partition(arr, lo, hi);
+    // sort up to left of pivot
+    if (lo < index - 1) quickSort(arr, lo, index - 1);
+    // sort from right of the pivot
+    if (index < hi) quickSort(arr, index, hi);
+  }
+  return arr;
+}
+
+const arr = [8,6,4,2,10,11,25,28,4,9,56,78,23];
+console.log(JSON.stringify(quickSort(arr, 0, arr.length-1)));
+
